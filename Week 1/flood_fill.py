4dc0bbed86b0a47002b9801416fc89d686e6e88b
Braulio Carrion Corveira <brauliocarrion@gmail.com>
2024-02-01 19:24:47 -0500

    Flood Fill

--- Week 1/flood_fill.py
+++ Week 1/flood_fill.py
@@ -4,15 +4,26 @@ class Solution:
     ) -> List[List[int]]:
         quadX = [sc + 1, sc - 1]
         quadY = [sr + 1, sr - 1]
+        cur = image[sr][sc]
 
-        image[sc][sr] = color
+        image[sr][sc] = color
 
         for x in quadX:
-            if x >= 0 and x < len(image) and image[sr][x]:
+            if (
+                x >= 0
+                and x < len(image[0])
+                and image[sr][x] == cur
+                and image[sr][x] != color
+            ):
                 image = self.floodFill(image, sr, x, color)
 
         for y in quadY:
-            if y >= 0 and y < len(image[0]):
+            if (
+                y >= 0
+                and y < len(image)
+                and image[y][sc] == cur
+                and image[y][sc] != color
+            ):
                 image = self.floodFill(image, y, sc, color)
 
         return image
