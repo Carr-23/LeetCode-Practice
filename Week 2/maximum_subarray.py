d09d7f762cf0a9f572d7c88c8bb5c9f49f262e19
Braulio Carrion Corveira <brauliocarrion@gmail.com>
2024-02-04 19:14:44 -0500

    Maximum Subarray

---
+++ Week 2/maximum_subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        i = 0
+        sum = 0
+        largestSum = nums[i]
+        for j in range(len(nums)):
+            sum += nums[j]
+
+            if nums[j] > sum:
+                i = j
+                sum = nums[j]
+
+            if sum > largestSum:
+                largestSum = sum
+
+            # if j > largestSum:
+            #     sum -= nums[i]
+            #     i += 1
+            #     largestSum = sum
+            #     print('te')
+
+        return largestSum
